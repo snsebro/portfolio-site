d758b76cf6c8439da5e0b30b70613558d19ebc6c
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-07-11 22:09:24 -0400

    project page component and css

--- src/Pages/Project-Page/ProjectPage.jsx
+++ src/Pages/Project-Page/ProjectPage.jsx
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./ProjectPage.css";
 
 export default class ProjectPage extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <>
+        <header>Header Stuff here</header>
+        <div className="main">
+          <div className="shayne"></div>
+          <div className="projects-container">
+            <div className="project-header">
+              <div className="header-left"></div>
+              <div className="header-mid"></div>
+              <div className="header-right"></div>
+            </div>
+
+
+
+            <div className="projects">
+              <div className="project-name"></div>
+              <div className="project-content"></div>
+              <div className="project-details"></div>
+            </div>
+          </div>
+          <div className="space"></div>
+        </div>
+      </>
+    );
   }
 }
